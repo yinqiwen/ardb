1a0a5c7f858475fcb2817b9750a25ab9418d83a2
yinqiwen <yinqiwen@gmail.com>
2013-05-19 15:56:51 +0800

    fix lmdb engine

--- src/engine/lmdb_engine.hpp
+++ src/engine/lmdb_engine.hpp
@@ -11,6 +11,7 @@
 #include "lmdb.h"
 #include "ardb.hpp"
 #include "util/config_helper.hpp"
+#include "util/thread/thread_local.hpp"
 #include <stack>
 
 namespace ardb
@@ -19,7 +20,7 @@ namespace ardb
 	class LMDBIterator: public Iterator
 	{
 		private:
-			LMDBEngine *m_engine;
+			MDB_txn *m_txn;
 			MDB_cursor * m_cursor;
 			MDB_val m_key;
 			MDB_val m_value;
@@ -32,9 +33,8 @@ namespace ardb
 			void SeekToFirst();
 			void SeekToLast();
 		public:
-			LMDBIterator(LMDBEngine *engine, MDB_cursor* iter,
-			        bool valid = true) :
-					m_engine(engine), m_cursor(iter), m_valid(valid)
+			LMDBIterator(MDB_txn * txn, MDB_cursor* iter, bool valid = true) :
+					m_txn(txn), m_cursor(iter), m_valid(valid)
 			{
 				if (valid)
 				{
@@ -61,14 +61,41 @@ namespace ardb
 		private:
 			MDB_env *m_env;
 			MDB_dbi m_dbi;
-			MDB_txn *m_txn;
-			uint32 m_batch_size;
-			std::stack<bool> m_batch_stack;
+			struct BatchHolder
+			{
+					MDB_txn *txn;
+					uint32 ref;
+					void ReleaseRef()
+					{
+						if (ref > 0)
+							ref--;
+					}
+					void AddRef()
+					{
+						ref++;
+					}
+					bool EmptyRef()
+					{
+						return ref == 0;
+					}
+					void Clear()
+					{
+						if (NULL != txn)
+						{
+
+						}
+					}
+					BatchHolder() :
+							txn(NULL), ref(0)
+					{
+					}
+			};
+			ThreadLocal<BatchHolder> m_batch_local;
 			std::string m_db_path;
 
 			LMDBConfig m_cfg;
 			friend class LevelDBEngineFactory;
-			int FlushWriteBatch();
+			int FlushWriteBatch(BatchHolder& holder);
 		public:
 			LMDBEngine();
 			~LMDBEngine();
