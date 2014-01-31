c8f6f7d11a4eac9b15631f6cca971dbdb54cfcb7
Pedro Larroy <pedro.larroy@gmail.com>
2014-01-31 17:26:30 +0100

    fix indentation

--- src/channel/codec/redis_reply.hpp
+++ src/channel/codec/redis_reply.hpp
@@ -1,10 +1,10 @@
 /*
  *Copyright (c) 2013-2013, yinqiwen <yinqiwen@gmail.com>
  *All rights reserved.
- * 
+ *
  *Redistribution and use in source and binary forms, with or without
  *modification, are permitted provided that the following conditions are met:
- * 
+ *
  *  * Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  *  * Redistributions in binary form must reproduce the above copyright
@@ -13,17 +13,17 @@
  *  * Neither the name of Redis nor the names of its contributors may be used
  *    to endorse or promote products derived from this software without
  *    specific prior written permission.
- * 
+ *
  *THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  *AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  *IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- *ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS 
+ *ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
  *BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
  *CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
  *SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
  *INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
  *CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- *ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ *ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  *THE POSSIBILITY OF SUCH DAMAGE.
  */
 
@@ -47,65 +47,65 @@
 
 namespace ardb
 {
-	namespace codec
-	{
-		struct RedisDumpFileChunk
-		{
-				int64 len;
-				uint32 flag;
-				std::string chunk;
-				RedisDumpFileChunk() :
-						len(0), flag(0)
-				{
-				}
-				bool IsLastChunk()
-				{
-					return (flag & LAST_CHUNK_FLAG) == (LAST_CHUNK_FLAG);
-				}
-				bool IsFirstChunk()
-				{
-					return (flag & FIRST_CHUNK_FLAG) == (FIRST_CHUNK_FLAG);
-				}
-		};
+    namespace codec
+    {
+        struct RedisDumpFileChunk
+        {
+            int64 len;
+            uint32 flag;
+            std::string chunk;
+            RedisDumpFileChunk() :
+                len(0), flag(0)
+            {
+            }
+            bool IsLastChunk()
+            {
+                return (flag & LAST_CHUNK_FLAG) == (LAST_CHUNK_FLAG);
+            }
+            bool IsFirstChunk()
+            {
+                return (flag & FIRST_CHUNK_FLAG) == (FIRST_CHUNK_FLAG);
+            }
+        };
 
-		struct RedisReply
-		{
-				int type;
-				std::string str;
+        struct RedisReply
+        {
+            int type;
+            std::string str;
 
-				/*
-				 * If the type is REDIS_REPLY_STRING, and the str's length is large,
-				 * the integer value also used to identify chunk state.
-				 */
-				int64_t integer;
-				double double_value;
-				std::deque<RedisReply> elements;
-				RedisReply() :
-						type(0), integer(0), double_value(0)
-				{
-				}
-				RedisReply(uint64 v) :
-						type(REDIS_REPLY_INTEGER), integer(v), double_value(0)
-				{
-				}
-				RedisReply(double v) :
-						type(REDIS_REPLY_DOUBLE), integer(0), double_value(v)
-				{
-				}
-				RedisReply(const std::string& v) :
-						type(REDIS_REPLY_STRING), str(v), integer(0), double_value(0)
-				{
-				}
-				void Clear()
-				{
-					type = 0;
-					integer = 0;
-					double_value = 0;
-					str.clear();
-					elements.clear();
-				}
-		};
-	}
+            /*
+             * If the type is REDIS_REPLY_STRING, and the str's length is large,
+             * the integer value also used to identify chunk state.
+             */
+            int64_t integer;
+            double double_value;
+            std::deque<RedisReply> elements;
+            RedisReply() :
+                type(0), integer(0), double_value(0)
+            {
+            }
+            RedisReply(uint64 v) :
+                type(REDIS_REPLY_INTEGER), integer(v), double_value(0)
+            {
+            }
+            RedisReply(double v) :
+                type(REDIS_REPLY_DOUBLE), integer(0), double_value(v)
+            {
+            }
+            RedisReply(const std::string& v) :
+                type(REDIS_REPLY_STRING), str(v), integer(0), double_value(0)
+            {
+            }
+            void Clear()
+            {
+                type = 0;
+                integer = 0;
+                double_value = 0;
+                str.clear();
+                elements.clear();
+            }
+        };
+    }
 }
 
 #endif /* REDIS_REPLY_HPP_ */
