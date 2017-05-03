334b12f40d847ad122959dc06a6d4cf25c02e60d
yinqiwen <yinqiwen@gmail.com>
2017-05-03 01:14:51 -0600

    update

---
+++ src/module/module_data.h
@@ -0,0 +1,22 @@
+/*
+ * module_data.h
+ *
+ *  Created on: 2017Äê4ÔÂ27ÈÕ
+ *      Author: qiyingwang
+ */
+
+#ifndef SRC_MODULE_MODULE_DATA_H_
+#define SRC_MODULE_MODULE_DATA_H_
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#endif /* SRC_MODULE_MODULE_DATA_H_ */
